8a9b010756e4a8c5c610c0619074888d33c58e02
ccrshishir1 <shishirkc.ccr@gmail.com>
2019-02-01 21:37:20 -0600

    aws tags list first

---
+++ listtags.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Replace/Add your regions in below variable list
+region=("us-east-1" "us-east-2" "eu-west-3")
+outputFile="InstanceTagsList.csv"
+
+
+# Removing output file to overcome content overlap issues
+
+if [ -f "$outputFile" ]; then
+
+ rm -rf "$outputFile"
+fi
+
+# Getting List of instances into array and looping them..
+
+for instance in ${region[@]};
+do
+
+#AWS CLI  command to get all tags details of each instance in a region
+
+aws ec2 describe-tags --region $instance  --filters "Name=resource-type,Values=instance" | jq -r '.Tags[] | "\(.ResourceId),\(.Key),\(.Value)"' >> "$outputFile"
+done
+
+
+#Output
+
+#   <Instance ID>,<Tag Key>,<Tag Value>
+
+# This out put file will be stored into csv file as we redirected..
+
